2bb5bea33b56f00c1ddcc3a9945fc44fda17f123
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-03-11 14:07:07 +0100

    Use GetMin in main class

--- Cw2/Cw2/Program.cs
+++ Cw2/Cw2/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using Cw2;
 
 Console.WriteLine("Hello, World");
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int min = StatisticsHelper.GetMin(numbers);
+Console.WriteLine(min);
