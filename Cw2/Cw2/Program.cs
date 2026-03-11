0b35a2bc1db5c7ef762a60bad0f5ecb6f9619267
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-03-11 14:11:04 +0100

    Add error messsage

--- Cw2/Cw2/Program.cs
+++ Cw2/Cw2/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using Cw2;
+﻿using Cw2;
 
 Console.WriteLine("Hello, World");
 int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 int min = StatisticsHelper.GetMin(numbers);
 Console.WriteLine(min);
+if (numbers.Length == 0) Console.WriteLine("Error:No numbers found");
