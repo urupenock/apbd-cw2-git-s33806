bd81dc002a45f5179c1b86b630408f0d801d905b
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-03-11 14:04:01 +0100

    Add GetMin method

--- Cw2/Cw2/StatisticsHelper.cs
+++ Cw2/Cw2/StatisticsHelper.cs
@@ -2,5 +2,14 @@ namespace Cw2;
 
 public class StatisticsHelper
 {
-    
+    public static int GetMin(int[] numbers)
+    {
+        int min = numbers[0];
+        foreach (int n in numbers)
+        {
+            if (n < min)
+                min = n;
+        }
+        return min;
+    }
 }
