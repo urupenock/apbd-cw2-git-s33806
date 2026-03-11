b216cd4d34a9125f0218362484b641c109e82a5a
Anhelina Urupa <s33806@pjwstk.edu.pl>
2026-03-11 14:20:24 +0100

    Use GetAverage in main class

--- Cw2/Cw2/Program.cs
+++ Cw2/Cw2/Program.cs
@@ -5,3 +5,4 @@ int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 int min = StatisticsHelper.GetMin(numbers);
 Console.WriteLine(min);
 if (numbers.Length == 0) Console.WriteLine("Error:No numbers found");
+Console.WriteLine("Average: " + StatisticsHelper.GetAverage(numbers));
